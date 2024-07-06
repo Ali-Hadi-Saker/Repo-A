23122f2ecf4eb6c1e6d58a6da523d98c14972752
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-06 17:56:44 +0300

    read config.yaml file from parent folder

--- RepoA.py
+++ RepoA.py
@@ -1,2 +1,19 @@
-def repo_name():
-    print('hi Repo A')
+import yaml
+import os
+
+def main():
+    config = readConfig()
+    if config.get('run_localhost'):
+        print('ERROR !!!')
+
+
+
+main()
+
+def readConfig():
+    path = os.path.join(os.path.dirname(__file__), '..', 'config.yaml')
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+# def repo_name():
+#     print('hi Repo A')

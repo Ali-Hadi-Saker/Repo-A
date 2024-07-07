da8d080f360e9a56cb7dd1621e8e9544d83d7522
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-07 10:42:09 +0300

    change repo A

--- RepoA.py
+++ RepoA.py
@@ -1,14 +1,22 @@
 import yaml
-import os
+import requests
 
 def main():
-    path = os.path.join(os.path.dirname(__file__), '..', 'config.yaml')
-    with open(path, 'r') as file:
-        config = yaml.safe_load(file)
-    if config.get('run_localhost'):
-        print('ERROR !!!')
+    url = "https://github.com/Ali-Hadi-Saker/Repo-A-B/main/config.yaml"
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            print(response.text)
+            config = yaml.safe_load(response.text)
+            if config.get('run_localhost'):
+                print('ERROR !!!')
+        except yaml.YAMLError as exc:
+            print(f"Error parsing YAML: {exc}")
+    else:
+        print(f"Failed to fetch the config.yaml file. Status code: {response.status_code}")
 
 main()
+
 
 
 # def repo_name():

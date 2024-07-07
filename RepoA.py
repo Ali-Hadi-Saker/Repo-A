57f99ba634d908a2bd4f52097f68a00e99851d65
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-07 12:45:25 +0300

    restore previous version

--- RepoA.py
+++ RepoA.py
@@ -1,22 +1,14 @@
 import yaml
-import requests
+import os
 
 def main():
-    url = "https://github.com/Ali-Hadi-Saker/Repo-A-B/main/config.yaml"
-    response = requests.get(url)
-    if response.status_code == 200:
-        try:
-            print(response.text)
-            config = yaml.safe_load(response.text)
-            if config.get('run_localhost'):
-                print('ERROR !!!')
-        except yaml.YAMLError as exc:
-            print(f"Error parsing YAML: {exc}")
-    else:
-        print(f"Failed to fetch the config.yaml file. Status code: {response.status_code}")
+    path = os.path.join(os.path.dirname(__file__), '..', 'config.yaml')
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+    if config.get('run_localhost'):
+        print('ERROR !!!')
 
 main()
-
 
 
 # def repo_name():
